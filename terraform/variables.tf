f44f6c6e876dcaa4a4491515afa97b8dbcacb09d
Christian Studt <107218266+TeinerHD@users.noreply.github.com>
2024-09-18 10:56:30 +0200

    Update variables.tf
    Added Proxmox Secrets

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,11 @@
-variable "resource_group_name" {
-  default = "zabbix-rg"
+variable "proxmox_user" {
+  description = "The admin user for the Proxmox"
+  type        = string
+  sensitive   = true
 }
 
-variable "location" {
-  default = "Germany North"
-}
-
-variable "vm_size" {
-  default = "Standard_B1s"
-}
-
-variable "admin_username" {
-  default = "zabbixadmin"
-}
-
-variable "admin_password" {
-  description = "The admin password for the VM and Ansible"
+variable "proxmox_password" {
+  description = "The admin password for the Proxmox"
   type        = string
   sensitive   = true
 }
